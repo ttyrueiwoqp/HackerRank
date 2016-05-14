3f4fd4794bd0bfe17dbbeeed0b77632fb0e1ad6a
ttyrueiwoqp <lufangjiannus@gmail.com>
2016-05-14 12:32:18 +0800

    Initial commit

---
+++ src/game/theory/GameOfStones.java
@@ -0,0 +1,13 @@
+package game.theory;
+
+/**
+ * Created by lvfan on 5/14/2016.
+ */
+public class GameOfStones {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        
+    }
+}
