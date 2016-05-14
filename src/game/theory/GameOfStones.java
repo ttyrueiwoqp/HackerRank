5eaca1680d4fed9b52575d77338e934d266b6a54
ttyrueiwoqp <lufangjiannus@gmail.com>
2016-05-14 13:59:28 +0800

    Completed TowewrBreakers.

--- src/game/theory/GameOfStones.java
+++ src/game/theory/GameOfStones.java
@@ -1,13 +1,28 @@
 package game.theory;
 
-/**
- * Created by lvfan on 5/14/2016.
- */
+import java.util.Scanner;
+
 public class GameOfStones {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int[] dp = solve(100);
 
-        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            System.out.println(dp[sc.nextInt()] == 1 ? "First" : "Second");
+        }
+
+    }
+
+    private static int[] solve(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 2;
+        dp[2] = dp[3] = dp[4] = dp[5] = 1;
+        for (int i = 6; i < n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 3] + dp[i - 5] > 3 ? 1 : 2;
+        }
+        return dp;
     }
 }
---
+++ src/game/theory/TowerBreakers.java
@@ -0,0 +1,25 @@
+package game.theory;
+
+import java.util.Scanner;
+
+public class TowerBreakers {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            System.out.println(solve(n, m));
+        }
+
+    }
+
+    private static int solve(int n, int m) {
+        if (m == 1) {
+            return 2;
+        }
+        return 2 - n % 2;
+    }
+}
